4ede35824d862e627acec6d5db20bd4dd0087762
Yogesh <yogeshsuresh28@gmail.com>
2022-12-08 07:15:42 +0530

    Enemy Patrolling added

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,9 @@ public class EnemyController : MonoBehaviour
     private BoxCollider2D enemyCollider;
     PlayerController playercontroller;
 
+    private int speed = 2;
+    bool flipped = false;
+
     private void Start()
     {
         enemyCollider = gameObject.GetComponent<BoxCollider2D>();
@@ -16,7 +19,7 @@ public class EnemyController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.GetComponent<PlayerController>() != null)
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
             playercontroller = collision.gameObject.GetComponent<PlayerController>();
             playercontroller.EnemyCollider();
@@ -24,5 +27,35 @@ public class EnemyController : MonoBehaviour
             heartanim.SetTrigger("heartBreak");
             Destroy(GameObject.FindWithTag("Heart"), 0.5f);
         }
+
+        if (collision.gameObject.CompareTag("WayPoint"))
+        {
+            //Flip the enemy
+            if (flipped == false)
+            {
+                flipped = true;
+                Vector3 scale = transform.localScale;
+                scale.x = -1f * scale.x;
+                transform.localScale = scale;
+            }
+        }
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("WayPoint"))
+        {
+            flipped = false;
+        }
+    }
+    private void Update()
+    {
+        //Move in the direction that it is facing
+        Vector3 Lscale = transform.localScale;
+        Vector3 position = transform.position;
+        speed = (Lscale.x > 0f) ? Mathf.Abs(speed) : (-1* Mathf.Abs(speed));
+        position.x += speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+
 }
