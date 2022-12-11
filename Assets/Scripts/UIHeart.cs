fcd217bc0abd2507df5ec57e88c3cb9824322e3e
Yogesh <yogeshsuresh28@gmail.com>
2022-12-11 10:30:18 +0530

    animation added in script.

--- Assets/Scripts/UIHeart.cs
+++ Assets/Scripts/UIHeart.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class UIHeart : MonoBehaviour
 {
     private bool one, two, three;
+    private bool animationStop = false;
+    private float timer;
 
     private GameObject fullHeart1;
     private GameObject fullHeart2;
@@ -11,6 +13,9 @@ public class UIHeart : MonoBehaviour
     private GameObject emptyHeart1;
     private GameObject emptyHeart2;
     private GameObject emptyHeart3;
+
+    private Animator heartAnim;
+    private RectTransform heartTransform;
 
     private void Start()
     {
@@ -25,18 +30,47 @@ public class UIHeart : MonoBehaviour
     public void HeartController(int heartCount)
     {
         Debug.Log("Heart = "+ heartCount);
-        bool one = (heartCount >= 1) ? true : false;
-        bool two = (heartCount >= 2) ? true : false;
-        bool three = (heartCount >= 3) ? true : false;
+        one = (heartCount >= 1) ? true : false;
+        two = (heartCount >= 2) ? true : false;
+        three = (heartCount >= 3) ? true : false;
 
         Debug.Log(one);
         Debug.Log(two);
         Debug.Log(three);
-        fullHeart1.SetActive(three);
+
+        
+        /*fullHeart1.SetActive(three);
         emptyHeart1.SetActive(!three);
         fullHeart2.SetActive(two);
         emptyHeart2.SetActive(!two);
         fullHeart3.SetActive(one);
-        emptyHeart3.SetActive(!one);
+        emptyHeart3.SetActive(!one);*/
+    }
+
+    private void Update()
+    {
+       if(three == false && animationStop == false)
+        {
+            heartBrokeAnimation(fullHeart1);
+        }
+        
+    }
+
+    private void heartBrokeAnimation(GameObject heartObj)
+    {
+        heartAnim = heartObj.GetComponent<Animator>();
+        heartTransform = heartObj.GetComponent<RectTransform>();
+        //heartObj.transform.Translate(new Vector3(0,0,0), Space.World);
+        timer += Time.deltaTime;
+        if(timer >= 1f)
+        {
+            heartAnim.SetTrigger("HeartBroken");
+        }
+        if(timer >= 2.5f)
+        {
+            animationStop = true;
+            fullHeart1.SetActive(false);
+        }
+         
     }
 }
